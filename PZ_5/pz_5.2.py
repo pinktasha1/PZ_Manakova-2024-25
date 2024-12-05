5c432c9bd986d1f05f6e27e66f65311093002afc
pinktasha1 <nekowannafly1@gmail.ru>
2024-12-06 00:35:22 +0300

    выполнила работу

--- PZ_5/pz_5.2.py
+++ PZ_5/pz_5.2.py
@@ -2,55 +2,42 @@
 # а в переменную Y — максимальное из этих значений (X и Y — вещественные параметры,
 # являющиеся одновременно входными и выходными).
 # Используя четыре вызова этой функции, найти минимальное и максимальное из данных чисел A, B, C, D.
+from typing import final
 
-def min_max(x, y):
+
+def min_max(x, y): #x - минимальное значение, y - максимальное значение
     if x > y:
-        x , y = y, x #меняю порядок значений если x > y
+        x , y = y , x #меняю порядок значений если x > y
     return x, y
 
 
-#ввод переменных
-a = input('Введи число A: ')
-while type(a) != float: #обработка исключений
-  try:
-    a = float(a)
-  except ValueError :
-    print('Неправильно ввели!')
-    a = input('Введите число A заново, оно должно быть нецелым: ') #пусть пользователь введёт число заново
+def exception(var, message): #обработка исключений
+  while type(var) != float:
+    try:
+      var = float(var)
+    except ValueError:
+      print('Неправильно ввели!')
+      var = input(message)
+  return var
 
-b = input('Введи число B: ')
-while type(b) != float: #обработка исключений
-  try:
-    b = float(b)
-  except ValueError :
-    print('Неправильно ввели!')
-    b = input('Введите число B заново, оно должно быть нецелым: ') #пусть пользователь введёт число заново
 
-c = input('Введи число C: ')
-while type(c) != float: #обработка исключений
-  try:
-    c = float(c)
-  except ValueError :
-    print('Неправильно ввели!')
-    c = input('Введите число C заново, оно должно быть нецелым: ') #пусть пользователь введёт число заново
+a = input('Введи число a: ')
+a = exception(a, "Введи число заново")
 
-d = input('Введи число D: ')
-while type(d) != float: #обработка исключений
-  try:
-    d = float(d)
-  except ValueError :
-    print('Неправильно ввели!')
-    d = input('Введите число D заново, оно должно быть нецелым: ') #пусть пользователь введёт число заново
+b = input('Введи число b: ')
+b = exception(b, "Введи число заново")
+
+c = input('Введи число b: ')
+c = exception(c, "Введи число заново")
+
+d = input('Введи число b: ')
+d = exception(d, "Введи число заново")
 
 #нахожу пары минимальных и максимальных значений
-min_ab = min_max(a, b)[0]
-max_ab = min_max(a, b)[1]
-min_cd = min_max(c, d)[0]
-max_cd = min_max(c, d)[1]
-
+min_ab, max_ab = min_max(a, b)
+min_cd, max_cd = min_max(c, d)
 #нахожу минимум и максимум из пар
-min = min_max(min_cd, min_cd)[0]
-max = min_max(max_cd, max_cd)[1]
-#у меня не вышло сделать это в 4 операции:(
+final_min, _ = min_max(min_ab, min_cd)
+_, final_max = min_max(max_cd, max_ab)
 
-print(f'Минимальное число: {min}\nМаксимальное число: {max}')
+print(f'Минимальное число: {final_min}\nМаксимальное число: {final_max}')
