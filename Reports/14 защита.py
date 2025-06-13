41efea0a2aec6f2344125e7fb0e624c0bf98d0e9
pinktasha1 <nekowannafly1@gmail.ru>
2025-06-13 12:49:04 +0300

    выполнила работу

--- Reports/14 защита.py
+++ Reports/14 защита.py
@@ -1,14 +1,4 @@
-#напишите программу на пайтон которая считывает текстовый файл с именем 'ips.txt',
-#находит в нём все корректные IPv4-адреса(например 192.168.1.1)
-#и выводит их в консоль, каждый на новой строке.
-# если файл не существует или не содержит корректных ip-адресов, программа должна выводить соответствующие сообщения
-
 import re
-
-
-def file_exists(filename):
-    if FileExistsError(filename):
-        print('Файла не существует')
 
 
 def is_ipv4(ip):
@@ -19,16 +9,19 @@ def is_ipv4(ip):
         return True
 
 
-with open('ips.txt', 'r') as f:
-    text = f.read()
+try:
+    with open('ips.txt', 'r') as f:
+        text = f.read()
 
-find = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
-ip_addresses = re.findall(find, text)
-correct_ipv4 = [ip for ip in ip_addresses if is_ipv4(ip)]
+    find = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+    ip_addresses = re.findall(find, text)
+    correct_ipv4 = [ip for ip in ip_addresses if is_ipv4(ip)]
 
-if correct_ipv4:
-    print("Найденные корректные IPv4-адреса:")
-    for ip in correct_ipv4:
-        print(ip)
-else:
-    print('Корректные IP-адреса не найдены')
+    if correct_ipv4:
+        print("Найденные корректные IPv4-адреса:")
+        for ip in correct_ipv4:
+            print(ip)
+    else:
+        print('Корректные IP-адреса не найдены')
+except  FileNotFoundError:
+    print('Файл не найден')
